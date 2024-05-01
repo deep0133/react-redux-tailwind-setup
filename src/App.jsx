c86bfac61a237bb22514a7aa97c382bc014e4f26
deepanshuHeer <deepanshuheer72@gmail.com>
2024-05-01 16:00:18 +0530

    tailwind setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { useGetUsersQuery } from "./redux/slices/userSlice";
-
 function App() {
-  const { data, error, isLoading } = useGetUsersQuery();
-
-  console.log(isLoading);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error : {error.error}</div>;
-  }
   return (
     <>
-      <div>
-        <div>
-          <h3>User Data :</h3>
-          {data.map((user) => {
-            return <div key={user.id}>{user.name}</div>;
-          })}
-        </div>
+      <div className='bg-red-500'>
+        <h3> App.js </h3>
       </div>
     </>
   );
